a7b6474d8f69d6f28dddbad4a1f65049934b81e0
masashi matsubuchi <masashi.matsubuchi@gmail.com>
2026-02-18 00:37:24 +0900

    Remove max* googleBot robots directives

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,9 +67,6 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      maxSnippet: -1,
-      maxImagePreview: "large",
-      maxVideoPreview: -1,
     },
   },
   verification: {
